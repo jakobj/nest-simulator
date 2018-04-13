dbf4e8acd4bd16ae33432eeb59ad00358f3f6d9d
Itaru Kitayama <itaru.kitayama@riken.jp>
2018-04-13 13:02:04 +0900

    Comment out unused modules for the hpc_benchmark model
    Conflicts:
    	models/modelsmodule.cpp

--- models/modelsmodule.cpp
+++ models/modelsmodule.cpp
@@ -43,22 +43,21 @@
 #include "config.h"
 
 // Neuron models
-#include "aeif_cond_alpha.h"
-#include "aeif_cond_alpha_multisynapse.h"
-#include "aeif_cond_beta_multisynapse.h"
-#include "aeif_cond_alpha_RK5.h"
-#include "aeif_cond_exp.h"
-#include "aeif_psc_alpha.h"
-#include "aeif_psc_exp.h"
-#include "aeif_psc_delta.h"
-#include "amat2_psc_exp.h"
-#include "erfc_neuron.h"
-#include "gauss_rate.h"
-#include "ginzburg_neuron.h"
-#include "hh_cond_exp_traub.h"
+//#include "aeif_cond_alpha.h"
+//#include "aeif_cond_alpha_multisynapse.h"
+//#include "aeif_cond_beta_multisynapse.h"
+//#include "aeif_cond_alpha_RK5.h"
+//#include "aeif_cond_exp.h"
+//#include "aeif_psc_alpha.h"
+//#include "aeif_psc_exp.h"
+//#include "aeif_psc_delta.h"
+//#include "amat2_psc_exp.h"
+//#include "gauss_rate.h"
+//#include "ginzburg_neuron.h"
+//#include "hh_cond_exp_traub.h"
 #include "hh_psc_alpha.h"
 #include "hh_psc_alpha_gap.h"
-#include "ht_neuron.h"
+//#include "ht_neuron.h"
 #include "iaf_chs_2007.h"
 #include "iaf_chxk_2008.h"
 #include "iaf_cond_alpha.h"
@@ -71,23 +70,22 @@
 #include "iaf_psc_exp.h"
 #include "iaf_psc_exp_multisynapse.h"
 #include "iaf_tum_2000.h"
-#include "izhikevich.h"
-#include "lin_rate.h"
-#include "tanh_rate.h"
-#include "threshold_lin_rate.h"
-#include "mat2_psc_exp.h"
-#include "mcculloch_pitts_neuron.h"
-#include "parrot_neuron.h"
-#include "pp_pop_psc_delta.h"
-#include "pp_psc_delta.h"
-#include "siegert_neuron.h"
-#include "sigmoid_rate.h"
-#include "sigmoid_rate_gg_1998.h"
-#include "gif_psc_exp.h"
-#include "gif_psc_exp_multisynapse.h"
-#include "gif_cond_exp.h"
-#include "gif_cond_exp_multisynapse.h"
-#include "gif_pop_psc_exp.h"
+//#include "izhikevich.h"
+//#include "lin_rate.h"
+//#include "tanh_rate.h"
+//#include "threshold_lin_rate.h"
+//#include "mat2_psc_exp.h"
+//#include "mcculloch_pitts_neuron.h"
+//#include "parrot_neuron.h"
+//#include "pp_pop_psc_delta.h"
+//#include "pp_psc_delta.h"
+//#include "siegert_neuron.h"
+//#include "sigmoid_rate.h"
+//#include "sigmoid_rate_gg_1998.h"
+//#include "gif_psc_exp.h"
+//#include "gif_psc_exp_multisynapse.h"
+//#include "gif_cond_exp.h"
+//#include "gif_cond_exp_multisynapse.h"
 
 // Stimulation devices
 #include "ac_generator.h"
@@ -192,39 +190,39 @@ void
 ModelsModule::init( SLIInterpreter* )
 {
   // rate models with input noise
-  kernel().model_manager.register_node_model< gauss_rate_ipn >(
-    "gauss_rate_ipn" );
-  kernel().model_manager.register_node_model< lin_rate_ipn >( "lin_rate_ipn" );
-  kernel().model_manager.register_node_model< sigmoid_rate_ipn >(
-    "sigmoid_rate_ipn" );
-  kernel().model_manager.register_node_model< sigmoid_rate_gg_1998_ipn >(
-    "sigmoid_rate_gg_1998_ipn" );
-  kernel().model_manager.register_node_model< tanh_rate_ipn >(
-    "tanh_rate_ipn" );
-  kernel().model_manager.register_node_model< threshold_lin_rate_ipn >(
-    "threshold_lin_rate_ipn" );
+  //kernel().model_manager.register_node_model< gauss_rate_ipn >(
+  //  "gauss_rate_ipn" );
+  //kernel().model_manager.register_node_model< lin_rate_ipn >( "lin_rate_ipn" );
+  //kernel().model_manager.register_node_model< sigmoid_rate_ipn >(
+  //  "sigmoid_rate_ipn" );
+  //kernel().model_manager.register_node_model< sigmoid_rate_gg_1998_ipn >(
+  //  "sigmoid_rate_gg_1998_ipn" );
+  //kernel().model_manager.register_node_model< tanh_rate_ipn >(
+  //  "tanh_rate_ipn" );
+  //kernel().model_manager.register_node_model< threshold_lin_rate_ipn >(
+  //  "threshold_lin_rate_ipn" );
 
   // rate models with output noise
-  kernel().model_manager.register_node_model< lin_rate_opn >( "lin_rate_opn" );
-  kernel().model_manager.register_node_model< tanh_rate_opn >(
-    "tanh_rate_opn" );
-  kernel().model_manager.register_node_model< threshold_lin_rate_opn >(
-    "threshold_lin_rate_opn" );
+  //kernel().model_manager.register_node_model< lin_rate_opn >( "lin_rate_opn" );
+  //kernel().model_manager.register_node_model< tanh_rate_opn >(
+  //  "tanh_rate_opn" );
+  //kernel().model_manager.register_node_model< threshold_lin_rate_opn >(
+  //  "threshold_lin_rate_opn" );
 
   // rate transformer nodes
-  kernel().model_manager.register_node_model< rate_transformer_gauss >(
-    "rate_transformer_gauss" );
-  kernel().model_manager.register_node_model< rate_transformer_lin >(
-    "rate_transformer_lin" );
-  kernel().model_manager.register_node_model< rate_transformer_sigmoid >(
-    "rate_transformer_sigmoid" );
-  kernel()
-    .model_manager.register_node_model< rate_transformer_sigmoid_gg_1998 >(
-      "rate_transformer_sigmoid_gg_1998" );
-  kernel().model_manager.register_node_model< rate_transformer_tanh >(
-    "rate_transformer_tanh" );
-  kernel().model_manager.register_node_model< rate_transformer_threshold_lin >(
-    "rate_transformer_threshold_lin" );
+  //kernel().model_manager.register_node_model< rate_transformer_gauss >(
+  //  "rate_transformer_gauss" );
+  //kernel().model_manager.register_node_model< rate_transformer_lin >(
+  //  "rate_transformer_lin" );
+  //kernel().model_manager.register_node_model< rate_transformer_sigmoid >(
+  //  "rate_transformer_sigmoid" );
+  //kernel()
+  //  .model_manager.register_node_model< rate_transformer_sigmoid_gg_1998 >(
+  //    "rate_transformer_sigmoid_gg_1998" );
+  //kernel().model_manager.register_node_model< rate_transformer_tanh >(
+  //  "rate_transformer_tanh" );
+  //kernel().model_manager.register_node_model< rate_transformer_threshold_lin >(
+  //  "rate_transformer_threshold_lin" );
 
   kernel().model_manager.register_node_model< iaf_chs_2007 >( "iaf_chs_2007" );
   kernel().model_manager.register_node_model< iaf_psc_alpha >(
@@ -237,48 +235,45 @@ ModelsModule::init( SLIInterpreter* )
   kernel().model_manager.register_node_model< iaf_psc_exp_multisynapse >(
     "iaf_psc_exp_multisynapse" );
   kernel().model_manager.register_node_model< iaf_tum_2000 >( "iaf_tum_2000" );
-  kernel().model_manager.register_node_model< amat2_psc_exp >(
-    "amat2_psc_exp" );
-  kernel().model_manager.register_node_model< mat2_psc_exp >( "mat2_psc_exp" );
-  kernel().model_manager.register_node_model< parrot_neuron >(
-    "parrot_neuron" );
-  kernel().model_manager.register_node_model< pp_psc_delta >( "pp_psc_delta" );
-  kernel().model_manager.register_node_model< pp_pop_psc_delta >(
-    "pp_pop_psc_delta" );
-  kernel().model_manager.register_node_model< gif_psc_exp >( "gif_psc_exp" );
-  kernel().model_manager.register_node_model< gif_psc_exp_multisynapse >(
-    "gif_psc_exp_multisynapse" );
+  //kernel().model_manager.register_node_model< amat2_psc_exp >(
+  //  "amat2_psc_exp" );
+  //kernel().model_manager.register_node_model< mat2_psc_exp >( "mat2_psc_exp" );
+  //kernel().model_manager.register_node_model< parrot_neuron >(
+  //  "parrot_neuron" );
+  //kernel().model_manager.register_node_model< pp_psc_delta >( "pp_psc_delta" );
+  //kernel().model_manager.register_node_model< pp_pop_psc_delta >(
+  //  "pp_pop_psc_delta" );
+  //kernel().model_manager.register_node_model< gif_psc_exp >( "gif_psc_exp" );
+  //kernel().model_manager.register_node_model< gif_psc_exp_multisynapse >(
+  //  "gif_psc_exp_multisynapse" );
 
-  kernel().model_manager.register_node_model< ac_generator >( "ac_generator" );
-  kernel().model_manager.register_node_model< dc_generator >( "dc_generator" );
-  kernel().model_manager.register_node_model< spike_generator >(
-    "spike_generator" );
-  kernel().model_manager.register_node_model< inhomogeneous_poisson_generator >(
-    "inhomogeneous_poisson_generator" );
-  kernel().model_manager.register_node_model< poisson_generator >(
-    "poisson_generator" );
-  kernel().model_manager.register_node_model< pulsepacket_generator >(
-    "pulsepacket_generator" );
-  kernel().model_manager.register_node_model< noise_generator >(
-    "noise_generator" );
-  kernel().model_manager.register_node_model< step_current_generator >(
-    "step_current_generator" );
-  kernel().model_manager.register_node_model< mip_generator >(
-    "mip_generator" );
-  kernel().model_manager.register_node_model< sinusoidal_poisson_generator >(
-    "sinusoidal_poisson_generator" );
-  kernel().model_manager.register_node_model< ppd_sup_generator >(
-    "ppd_sup_generator" );
-  kernel().model_manager.register_node_model< gamma_sup_generator >(
-    "gamma_sup_generator" );
-  kernel().model_manager.register_node_model< erfc_neuron >( "erfc_neuron" );
-  kernel().model_manager.register_node_model< ginzburg_neuron >(
-    "ginzburg_neuron" );
-  kernel().model_manager.register_node_model< mcculloch_pitts_neuron >(
-    "mcculloch_pitts_neuron" );
-  kernel().model_manager.register_node_model< izhikevich >( "izhikevich" );
-  kernel().model_manager.register_node_model< spike_dilutor >(
-    "spike_dilutor" );
+  //kernel().model_manager.register_node_model< ac_generator >( "ac_generator" );
+  //kernel().model_manager.register_node_model< dc_generator >( "dc_generator" );
+  //kernel().model_manager.register_node_model< spike_generator >(
+  //  "spike_generator" );
+  //kernel().model_manager.register_node_model< poisson_generator >(
+  //  "poisson_generator" );
+  //kernel().model_manager.register_node_model< pulsepacket_generator >(
+  //  "pulsepacket_generator" );
+  //kernel().model_manager.register_node_model< noise_generator >(
+  //  "noise_generator" );
+  //kernel().model_manager.register_node_model< step_current_generator >(
+  //  "step_current_generator" );
+  //kernel().model_manager.register_node_model< mip_generator >(
+  //  "mip_generator" );
+  //kernel().model_manager.register_node_model< sinusoidal_poisson_generator >(
+  //  "sinusoidal_poisson_generator" );
+  //kernel().model_manager.register_node_model< ppd_sup_generator >(
+  //  "ppd_sup_generator" );
+  //kernel().model_manager.register_node_model< gamma_sup_generator >(
+  //  "gamma_sup_generator" );
+  //kernel().model_manager.register_node_model< ginzburg_neuron >(
+  //  "ginzburg_neuron" );
+  //kernel().model_manager.register_node_model< mcculloch_pitts_neuron >(
+  //  "mcculloch_pitts_neuron" );
+  //kernel().model_manager.register_node_model< izhikevich >( "izhikevich" );
+  //kernel().model_manager.register_node_model< spike_dilutor >(
+  //  "spike_dilutor" );
 
   kernel().model_manager.register_node_model< spike_detector >(
     "spike_detector" );
@@ -384,39 +379,37 @@ ModelsModule::init( SLIInterpreter* )
   kernel().model_manager.register_node_model< hh_psc_alpha >( "hh_psc_alpha" );
   kernel().model_manager.register_node_model< hh_psc_alpha_gap >(
     "hh_psc_alpha_gap" );
-  kernel().model_manager.register_node_model< hh_cond_exp_traub >(
-    "hh_cond_exp_traub" );
+  //kernel().model_manager.register_node_model< hh_cond_exp_traub >(
+  //  "hh_cond_exp_traub" );
   kernel().model_manager.register_node_model< sinusoidal_gamma_generator >(
     "sinusoidal_gamma_generator" );
-  kernel().model_manager.register_node_model< gif_cond_exp >( "gif_cond_exp" );
-  kernel().model_manager.register_node_model< gif_cond_exp_multisynapse >(
-    "gif_cond_exp_multisynapse" );
-  kernel().model_manager.register_node_model< gif_pop_psc_exp >(
-    "gif_pop_psc_exp" );
+  //kernel().model_manager.register_node_model< gif_cond_exp >( "gif_cond_exp" );
+  //kernel().model_manager.register_node_model< gif_cond_exp_multisynapse >(
+  //  "gif_cond_exp_multisynapse" );
 
-  kernel().model_manager.register_node_model< aeif_cond_alpha >(
-    "aeif_cond_alpha" );
-  kernel().model_manager.register_node_model< aeif_cond_exp >(
-    "aeif_cond_exp" );
-  kernel().model_manager.register_node_model< aeif_psc_alpha >(
-    "aeif_psc_alpha" );
-  kernel().model_manager.register_node_model< aeif_psc_exp >( "aeif_psc_exp" );
-  kernel().model_manager.register_node_model< aeif_psc_delta >(
-    "aeif_psc_delta" );
-  kernel().model_manager.register_node_model< ht_neuron >( "ht_neuron" );
-  kernel().model_manager.register_node_model< aeif_cond_beta_multisynapse >(
-    "aeif_cond_beta_multisynapse" );
-  kernel().model_manager.register_node_model< aeif_cond_alpha_multisynapse >(
-    "aeif_cond_alpha_multisynapse" );
-  kernel().model_manager.register_node_model< siegert_neuron >(
-    "siegert_neuron" );
+  //kernel().model_manager.register_node_model< aeif_cond_alpha >(
+  //  "aeif_cond_alpha" );
+  //kernel().model_manager.register_node_model< aeif_cond_exp >(
+  //  "aeif_cond_exp" );
+  //kernel().model_manager.register_node_model< aeif_psc_alpha >(
+  //  "aeif_psc_alpha" );
+  //kernel().model_manager.register_node_model< aeif_psc_exp >( "aeif_psc_exp" );
+  //kernel().model_manager.register_node_model< aeif_psc_delta >(
+  //  "aeif_psc_delta" );
+  //kernel().model_manager.register_node_model< ht_neuron >( "ht_neuron" );
+  //kernel().model_manager.register_node_model< aeif_cond_beta_multisynapse >(
+  //  "aeif_cond_beta_multisynapse" );
+  //kernel().model_manager.register_node_model< aeif_cond_alpha_multisynapse >(
+  //  "aeif_cond_alpha_multisynapse" );
+  //kernel().model_manager.register_node_model< siegert_neuron >(
+  //  "siegert_neuron" );
 #endif
 
   // This version of the AdEx model does not depend on GSL.
-  kernel().model_manager.register_node_model< aeif_cond_alpha_RK5 >(
-    "aeif_cond_alpha_RK5",
-    /*private_model*/ false,
-    /*deprecation_info*/ "NEST 3.0" );
+  //kernel().model_manager.register_node_model< aeif_cond_alpha_RK5 >(
+  //  "aeif_cond_alpha_RK5",
+  //  /*private_model*/ false,
+  //  /*deprecation_info*/ "NEST 3.0" );
 
 #ifdef HAVE_MUSIC
   //// proxies for inter-application communication using MUSIC
